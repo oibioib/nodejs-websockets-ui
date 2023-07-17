044a5376f8d408d5ed27caf09dd6bd07164d20fc
oibioib <pavelbodal@gmail.com>
2023-07-17 18:23:21 +0200

    refactor: change config

---
+++ src/config/board.ts
@@ -0,0 +1,7 @@
+export const BOARD_SIZE = 10;
+
+export enum ATTACK_STATUS {
+  MISS = 'miss',
+  KILLED = 'killed',
+  SHOT = 'shot',
+}
--- src/config/message.ts
+++ src/config/message.ts
@@ -3,4 +3,14 @@ export const messageRequiredFields = ['type', 'data'] as const;
 export enum MESSAGE_TYPE {
   REG = 'reg',
   UPDATE_WINNERS = 'update_winners',
+  UPDATE_ROOM = 'update_room',
+  CREATE_ROOM = 'create_room',
+  ADD_USER_TO_ROOM = 'add_user_to_room',
+  CREATE_GAME = 'create_game',
+  ADD_SHIPS = 'add_ships',
+  START_GAME = 'start_game',
+  TURN = 'turn',
+  ATTACK = 'attack',
+  FINISH = 'finish',
+  RANDOM_ATTACK = 'randomAttack',
 }
